3c7232457981f110939aa85d12506bf867d8a702
jbaranec <jozef.baranec@gmail.com>
2024-04-19 09:32:09 +0200

    v8.0.0 documentation

---
+++ docs/en/8.0.0/html/navtreedata.js
@@ -0,0 +1,57 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PDFix SDK", "index.html", [
+    [ "PDFix SDK Documentation", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "PDFix", "^https://pdfix.net", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group___enumerations___p_d_fix___s_d_k.html#gga2175c96d14cb7833e09b0ae49e719f97abc86e23a7f2537e21c6b6cabb837a071",
+"group___enumerations___p_d_fix___s_d_k.html#ggab7f41c15de69ac2b0d25f00d103118dca30ba73fd6a7c09df1dc1b9aa25c584be",
+"group___structures___p_d_fix___s_d_k.html#ga4ac7a144e49d7b11536491bef7a7e76d",
+"struct_pde_text.html#a95daf2b48b5df6e740a541c59e54043c",
+"struct_pdf_doc.html#ae75539535bd564bc2dc370a9925274b2",
+"struct_pds_content_mark.html",
+"struct_ps_standard_authorization.html#a622c001444483645aa21c6fee2bee918"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ docs/en/8.0.0/html/struct_ps_account_authorization.js
@@ -0,0 +1,5 @@
+var struct_ps_account_authorization =
+[
+    [ "Authorize", "struct_ps_account_authorization.html#a9891d9d39491dc27142cc8cff5670b7c", null ],
+    [ "Reset", "struct_ps_account_authorization.html#a85d765d7b87ed7d209b8cb760782d363", null ]
+];
