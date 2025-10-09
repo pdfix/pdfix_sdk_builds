9f0d96c8d58eb13d45bc8a0103f643a1e3acee21
Jozef Baranec <jozef.baranec@gmail.com>
2025-10-09 13:54:14 +0200

    v8.7.5 documentation

---
+++ docs/en/8.7.5/html/search/variables_10.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['standard_5fattrs_0',['standard_attrs',['../struct___pdf_tags_params.html#a173e8a7e26e05685043fedfc1b1f5e77',1,'_PdfTagsParams']]],
+  ['stroke_5fcolor_1',['stroke_color',['../struct___pdf_color_state.html#ae0f212f37529168528090ab2b62ecc5c',1,'_PdfColorState::stroke_color'],['../struct___pde_color_state.html#a62164e14835f44e284ba445815b201da',1,'_PdeColorState::stroke_color']]],
+  ['stroke_5fopacity_2',['stroke_opacity',['../struct___pdf_color_state.html#a8127319dd58ca66f55b7b0b9d6aae510',1,'_PdfColorState::stroke_opacity'],['../struct___pde_color_state.html#a3278535aa9dc3e49236f1ba5310435d0',1,'_PdeColorState::stroke_opacity']]],
+  ['stroke_5ftype_3',['stroke_type',['../struct___pdf_color_state.html#a918c6f5c4b55e1887ff130fc555b19b9',1,'_PdfColorState::stroke_type'],['../struct___pde_color_state.html#af777e8c8e012698b9788ffdec0606149',1,'_PdeColorState::stroke_type']]]
+];
