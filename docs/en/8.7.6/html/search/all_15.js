ffc451cd5b24679c518888ee0acb3a1ba5c73e42
Jozef Baranec <jozef.baranec@gmail.com>
2025-10-16 22:45:29 +0200

    8.7.6 documentation

---
+++ docs/en/8.7.6/html/search/all_15.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['validchild_0',['ValidChild',['../struct_pds_struct_element.html#a4e2a08629996d06a89888714da1ec8cc',1,'PdsStructElement']]],
+  ['vertical_1',['vertical',['../struct___pdf_font_state.html#ab9dddb912ca926972e7d7d2c0658f523',1,'_PdfFontState']]]
+];
---
+++ docs/en/8.7.6/html/search/functions_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['opendoc_0',['OpenDoc',['../struct_pdfix.html#ae5685d028d1ce149ebdbe1f37afbfd85',1,'Pdfix']]],
+  ['opendocfromstream_1',['OpenDocFromStream',['../struct_pdfix.html#a6402df0d430bb0fe9c0b6b2d6c7850c0',1,'Pdfix']]]
+];
