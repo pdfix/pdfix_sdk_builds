7109284210e5ad81b6e2647375718c1cc8d91fd8
Jozef Baranec <jozef.baranec@gmail.com>
2025-12-17 13:41:23 +0100

    v8.7.8 documentation

---
+++ docs/en/8.7.8/html/search/variables_f.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['r_0',['r',['../struct___pdf_r_g_b.html#aaccf7728e54e0fb4414c8e6445d67d64',1,'_PdfRGB']]],
+  ['render_5fflags_1',['render_flags',['../struct___pdf_page_render_params.html#a754daf81e8fe2681281d3374654760b0',1,'_PdfPageRenderParams::render_flags'],['../struct___pdf_tiff_params.html#a145c67ee6b5be5130a8ce9f89b62bff5',1,'_PdfTiffParams::render_flags']]],
+  ['revision_2',['revision',['../struct___pdf_standard_security_params.html#add3097642befdd82628a601ae2f7d9e1',1,'_PdfStandardSecurityParams']]],
+  ['right_3',['right',['../struct___pdf_rect.html#a11ae7cea540dd6768b526cd68e603894',1,'_PdfRect::right'],['../struct___pdf_dev_rect.html#a4a38b74523d9af0b7c5e8d61bf283457',1,'_PdfDevRect::right']]]
+];
