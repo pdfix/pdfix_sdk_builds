3dd010adeecadd33ff2106a7fa037018cf21f26c
Jozef Baranec <jozef.baranec@gmail.com>
2026-01-10 21:26:01 +0100

    documentation v8.7.10

---
+++ docs/en/8.7.10/html/search/all_e.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['opacity_0',['opacity',['../struct___pdf_annot_appearance.html#adbdf78e0e0e502350b9667cc72c74aee',1,'_PdfAnnotAppearance']]],
+  ['opendoc_1',['OpenDoc',['../struct_pdfix.html#ae5685d028d1ce149ebdbe1f37afbfd85',1,'Pdfix']]],
+  ['opendocfromstream_2',['OpenDocFromStream',['../struct_pdfix.html#a6402df0d430bb0fe9c0b6b2d6c7850c0',1,'Pdfix']]]
+];
---
+++ docs/en/8.7.10/html/search/functions_e.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['paste_0',['Paste',['../struct_pdf_action.html#ad9326f0fbf0132c992137bb30eb93943',1,'PdfAction::Paste()'],['../struct_pdf_annot.html#ae4545355efbeeaefa64bef5d364c7814',1,'PdfAnnot::Paste(PdfPage *dest_page, PdfPoint *center, void *data)=0']]],
+  ['pointinannot_1',['PointInAnnot',['../struct_pdf_annot.html#a4b16a4c466eef5125854e90bcba42e1a',1,'PdfAnnot']]],
+  ['pointtodevice_2',['PointToDevice',['../struct_pdf_page_view.html#a1b09261f1ca9be98fe08eca0d8c88fee',1,'PdfPageView']]],
+  ['pointtopage_3',['PointToPage',['../struct_pdf_page_view.html#afc73415a0eb82230dcab235b1bfd043e',1,'PdfPageView']]],
+  ['put_4',['Put',['../struct_pds_array.html#ab39831442a3718657a04cea3ab06ab8d',1,'PdsArray::Put()'],['../struct_pds_dictionary.html#abdb7e0652b0fe9c70e1d10448fbf4661',1,'PdsDictionary::Put(const wchar_t *key, PdsObject *value)=0']]],
+  ['putarray_5',['PutArray',['../struct_pds_dictionary.html#a76d7fc4905a6d3dfd2f34dd2fe28ae13',1,'PdsDictionary']]],
+  ['putbool_6',['PutBool',['../struct_pds_dictionary.html#a68200fd6f25492df740e825415fad6d3',1,'PdsDictionary']]],
+  ['putdict_7',['PutDict',['../struct_pds_dictionary.html#a49ec403476e73098522511bda96423c1',1,'PdsDictionary']]],
+  ['putmatrix_8',['PutMatrix',['../struct_pds_dictionary.html#a29f4c56164f85440c868827a32d97308',1,'PdsDictionary']]],
+  ['putname_9',['PutName',['../struct_pds_array.html#a93e94babaf6f419faa84b7effa23724c',1,'PdsArray::PutName()'],['../struct_pds_dictionary.html#a4468c5f90703e4c9365ebbe61485368a',1,'PdsDictionary::PutName()']]],
+  ['putnumber_10',['PutNumber',['../struct_pds_array.html#af88da39b9ecb8fa94878c642703cf6a8',1,'PdsArray::PutNumber()'],['../struct_pds_dictionary.html#a7c254f22d9fe667316f90b1da6bbd0f5',1,'PdsDictionary::PutNumber(const wchar_t *key, float value)=0']]],
+  ['putrect_11',['PutRect',['../struct_pds_dictionary.html#a4690b1bb40264ac275cde3c8ee6f818a',1,'PdsDictionary']]],
+  ['putstring_12',['PutString',['../struct_pds_array.html#afed481450fb3196c3d1fd8045e1e5263',1,'PdsArray::PutString()'],['../struct_pds_dictionary.html#aac38660e8a1f41bc99f6825bd20105e0',1,'PdsDictionary::PutString()']]]
+];
